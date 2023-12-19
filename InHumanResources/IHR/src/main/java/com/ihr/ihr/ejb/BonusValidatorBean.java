691dc7348d1658ef1e2c719db7a7af1260a3fd46
Darius15222 <dariusonofrei.2014@gmail.com>
2023-12-19 09:09:17 +0200

    added logger in bean

--- InHumanResources/IHR/src/main/java/com/ihr/ihr/ejb/BonusValidatorBean.java
+++ InHumanResources/IHR/src/main/java/com/ihr/ihr/ejb/BonusValidatorBean.java
@@ -4,20 +4,27 @@ import com.ihr.ihr.common.dtos.BonusDto;
 import com.ihr.ihr.common.interf.BonusValidation;
 import jakarta.ejb.Stateless;
 
+import java.util.logging.Logger;
+
 @Stateless
 public class BonusValidatorBean implements BonusValidation {
+
+    private static final Logger LOG = Logger.getLogger(BonusValidatorBean.class.getName());
     @Override
     public boolean ValueValidator(Integer value) {
+        LOG.info("ValueValidator");
         return value > 0;
     }
 
     @Override
     public boolean DenumireBonusValidator(String denumireBonus) {
+        LOG.info("DenumireBonusValidator");
         return !denumireBonus.isEmpty();
     }
 
     @Override
     public boolean BonusValidator(BonusDto bonusDto) {
+        LOG.info("BonusValidator");
         return ValueValidator(bonusDto.getValue()) && DenumireBonusValidator(bonusDto.getDenumireBonus());
     }
 }
