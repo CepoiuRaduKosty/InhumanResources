7bac7325306ba141b90303b49272a52213f508f9
Cepoiu_Radu_Konstantin <radu.cepoiu@gmail.com>
2023-12-29 15:26:04 +0200

    Updated PayDayProvider to use InvalidPayDayException

--- InHumanResources/IHR/src/main/java/com/ihr/ihr/common/interf/PayDayProvider.java
+++ InHumanResources/IHR/src/main/java/com/ihr/ihr/common/interf/PayDayProvider.java
@@ -1,12 +1,13 @@
 package com.ihr.ihr.common.interf;
 
+import com.ihr.ihr.common.excep.InvalidPayDayException;
 import com.ihr.ihr.common.excep.PayDayAlreadyExistsException;
 import com.ihr.ihr.common.excep.PayDayDoesNotExistException;
 import jakarta.validation.ValidationException;
 
 public interface PayDayProvider {
     Integer getDayOfMonth();
-    void addPayDayOfMonth(Integer dayOfMonth) throws ValidationException, PayDayAlreadyExistsException;
-    void updatePayDay(Integer dayOfMonth) throws ValidationException, PayDayDoesNotExistException;
+    void addPayDayOfMonth(Integer dayOfMonth) throws InvalidPayDayException, PayDayAlreadyExistsException;
+    void updatePayDay(Integer dayOfMonth) throws InvalidPayDayException, PayDayDoesNotExistException;
     boolean isPayDateSet();
 }
