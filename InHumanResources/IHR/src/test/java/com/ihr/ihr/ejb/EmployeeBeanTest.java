f849123e7c8a9d810f91c569dacfdec47a25eb1d
MarianAlexandruConstantin <alexandruconstantin.marian@ulbsibiu.ro>
2023-12-29 12:18:08 +0200

    Create EmployeeBeanTest.java

---
+++ InHumanResources/IHR/src/test/java/com/ihr/ihr/ejb/EmployeeBeanTest.java
@@ -0,0 +1,32 @@
+package com.ihr.ihr.ejb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeBeanTest {
+
+    @Test
+    void createEmployee() {
+    }
+
+    @Test
+    void deleteEmployeeById() {
+    }
+
+    @Test
+    void updateEmployeeById() {
+    }
+
+    @Test
+    void findAllEmployees() {
+    }
+
+    @Test
+    void findAllEmployeesByName() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
