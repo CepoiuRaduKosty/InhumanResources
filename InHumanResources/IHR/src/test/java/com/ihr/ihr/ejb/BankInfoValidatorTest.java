9b4c540694396abae41e72502c3c97e82318867e
Cepoiu_Radu_Konstantin <radu.cepoiu@gmail.com>
2023-12-30 17:36:42 +0200

    Fixed some issues with tests for BankInfoValidator

--- InHumanResources/IHR/src/test/java/com/ihr/ihr/ejb/BankInfoValidatorTest.java
+++ InHumanResources/IHR/src/test/java/com/ihr/ihr/ejb/BankInfoValidatorTest.java
@@ -5,49 +5,53 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import com.ihr.ihr.common.interf.BankInfoValidation;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class BankInfoValidatorTest {
-    private final BankInfoValidation tempBankInfoValidation=new BankInfoValidatorBean();
-    @Inject
-    BankInfoValidation bankInfoValidation;
+    @InjectMocks
+    BankInfoValidatorBean bankInfoValidatorBean;
     @Test
     void isBankNameValid_positive() {
         String name = "george";
-        boolean isValid = tempBankInfoValidation.isBankNameValid(name);
+        boolean isValid = bankInfoValidatorBean.isBankNameValid(name);
         assertTrue(isValid);
     }
 
     @Test
     void isBankNameValid_negative() {
         String name = "";
-        boolean isValid = tempBankInfoValidation.isBankNameValid(name);
+        boolean isValid = bankInfoValidatorBean.isBankNameValid(name);
         assertFalse(isValid);
     }
 
     @Test
     void isIBANValid_positive() {
         String IBAN ="RO49AAAA1B31007593840000";
-        boolean isValid = tempBankInfoValidation.isIBANValid(IBAN);
+        boolean isValid = bankInfoValidatorBean.isIBANValid(IBAN);
         assertTrue(isValid);
 
     }
     @Test
     void isIBANValid_negative() {
         String IBAN ="55";
-        boolean isValid = tempBankInfoValidation.isIBANValid(IBAN);
+        boolean isValid = bankInfoValidatorBean.isIBANValid(IBAN);
         assertFalse(isValid);
 
     }
     @Test
     void isBankInfoDtoValid_positive() {
         BankInfoDto validDto = new BankInfoDto(1L, "RO49AAAA1B31007593840000", "ValidBankName");
-        assertTrue(tempBankInfoValidation.isBankInfoDtoValid(validDto));
+        assertTrue(bankInfoValidatorBean.isBankInfoDtoValid(validDto));
     }
     @Test
     void isBankInfoDtoValid_negative() {
         BankInfoDto invalidDto = new BankInfoDto(1L, "RA1B31007593840000", "");
-        assertFalse(tempBankInfoValidation.isBankInfoDtoValid(invalidDto));
+        assertFalse(bankInfoValidatorBean.isBankInfoDtoValid(invalidDto));
     }
 }
