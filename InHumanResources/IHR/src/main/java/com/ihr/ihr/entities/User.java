9de6bc70ffe475024ec92f08c68f65704bb37bb2
Cepoiu_Radu_Konstantin <radu.cepoiu@gmail.com>
2024-01-07 14:01:53 +0200

    created user table and entity

---
+++ InHumanResources/IHR/src/main/java/com/ihr/ihr/entities/User.java
@@ -0,0 +1,59 @@
+package com.ihr.ihr.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @OneToOne
+    private Employee employee;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
