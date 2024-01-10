258861efbf04e5786848242c385ffe4d7bb99830
Cepoiu Radu Konstantin <radu.cepoiu@gmail.com>
2024-01-10 13:13:58 +0200

    restrictioned access to paycheck view page

--- InHumanResources/IHR/src/main/java/com/ihr/ihr/servlets/PaycheckViewServlet.java
+++ InHumanResources/IHR/src/main/java/com/ihr/ihr/servlets/PaycheckViewServlet.java
@@ -5,10 +5,8 @@ import com.ihr.ihr.common.dtos.PaycheckDtos.PaycheckDto;
 import com.ihr.ihr.common.dtos.PaymentInfoDtos.PaymentInfoDto;
 import com.ihr.ihr.common.enums.SalaryLevelEnum;
 import com.ihr.ihr.common.excep.UnknownPaycheckException;
-import com.ihr.ihr.common.interf.PaycheckBonusProvider;
-import com.ihr.ihr.common.interf.PaycheckProvider;
-import com.ihr.ihr.common.interf.PaymentInfoProvider;
-import com.ihr.ihr.common.interf.PaymentInfoValidation;
+import com.ihr.ihr.common.interf.*;
+import jakarta.annotation.security.DeclareRoles;
 import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -16,8 +14,12 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 
+@DeclareRoles({"EMPLOYEE", "ADMIN"})
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = {"ADMIN", "EMPLOYEE"}),
+        httpMethodConstraints = {@HttpMethodConstraint(value = "POST", rolesAllowed = {"ADMIN", "EMPLOYEE"})})
 @WebServlet(name = "PaycheckView", value = "/PaycheckView")
 public class PaycheckViewServlet extends HttpServlet {
     @Inject
@@ -28,6 +30,9 @@ public class PaycheckViewServlet extends HttpServlet {
 
     @Inject
     PaymentInfoProvider paymentInfoProvider;
+
+    @Inject
+    HTTPSessionManagement httpSessionManagement;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -46,6 +51,12 @@ public class PaycheckViewServlet extends HttpServlet {
             paycheckDto = paycheckProvider.getPaycheckById(paycheckId);
         } catch (UnknownPaycheckException e) {
             request.getRequestDispatcher("index.jsp").forward(request, response);
+            return;
+        }
+
+        Long paymentID = paycheckDto.getPaymentId();
+        if (!request.isUserInRole("ADMIN") && !Objects.equals(paymentID, httpSessionManagement.getPaymentIdLoggedIn(request))) {
+            request.getRequestDispatcher("/WEB-INF/pages/accessDenied.jsp").forward(request, response);
             return;
         }
 
